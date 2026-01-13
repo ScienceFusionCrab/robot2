e22a8a61a1645a4a48167424053fd0fae9ec430b
ScienceFusionCrab <mutarmutar7@gmail.com>
2026-01-13 15:10:45 -0500

    new2

--- include/autos.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "globals.h"
-
-namespace Robot{ namespace Autos{
-
-}}
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,135 @@
-#include "autos.h"
-#include "globals.h"
-#include "pros/misc.h"
-#include "ui.h"
 #include "main.h"
+#include "lemlib/chassis/chassis.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/adi.hpp"
+#include "pros/misc.h"
+#include "pros/misc.hpp"
+#include "pros/motors.h"
+#include "pros/rtos.hpp"
 
-using namespace Robot::Globals;
-using namespace Robot::Autos;
-using namespace Robot::Ui;
 
+	pros::MotorGroup groupL({16, -17, 18});
+	pros::MotorGroup groupR({13, -14, 15});
+	pros::MotorGroup chain({20, 9});		 //intake
+	pros::adi::DigitalOut hawk('G', false);	 //upsies downsies
+    bool hawkDown = false;
+	pros::Imu imu(12);
+	pros::adi::DigitalOut tuah('H', false);	//match loader
+    bool tuahDown = false;
+	pros::Motor therizzler(10);				//wedge
+	pros::adi::DigitalOut descore('F',false);
+	bool descoreDown = false;
 
-/**
- * A callback function for prLLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+	lemlib::Drivetrain drivetrain(&groupL, 	// left motor group
+        &groupR, 							// right motor group
+        10.5, 								// 10.5 inch track width
+        lemlib::Omniwheel::NEW_275, 		// using new 2.75" omnis
+        450, 								// drivetrain rpm is 360
+        2 									// horizontal drift is 2 (for now)
+	);
+
+	lemlib::ExpoDriveCurve steer_curve(3,	// joystick deadband out of 127
+        10, 								// minimum output where drivetrain will move out of 127
+        1 									// expo curve gain
+	);
+
+	lemlib::ExpoDriveCurve throttle_curve(3, 	// joystick deadband out of 127
+    	10, 									// minimum output where drivetrain will move out of 127
+    	1 										// expo curve gain
+	);
+
+	lemlib::ControllerSettings lateral_controller(
+		12,		// proportional gain (kP)
+        0, 		// integral gain (kI)
+        8, 		// derivative gain (kD)
+        3, 		// anti windup
+        1, 		// small error range, in inches
+        100, 	// small error range timeout, in milliseconds
+        3, 		// large error range, in inches
+        500, 	// large error range timeout, in milliseconds
+        20 		// maximum acceleration (slew)
+	);
+
+	lemlib::ControllerSettings angular_controller(
+		6, 		// proportional gain (kP)
+		0, 		// integral gain (kI)
+		55, 	// derivative gain (kD)
+		3, 		// anti windup
+		1, 		// small error range, in inches
+		100, 	// small error range timeout, in milliseconds
+		3, 		// large error range, in inches
+		500,	// large error range timeout, in milliseconds
+		0 		// maximum acceleration (slew)
+	);
+
+	lemlib::OdomSensors sensors(
+		nullptr, 	// vertical tracking wheel 1, set to null
+		nullptr, 	// vertical tracking wheel 2, set to nullptr as we are using IMEs
+		nullptr,
+		nullptr, 	// horizontal tracking wheel 2, set to nullptr as we don't have a second one
+		&imu 		// inertial sensor
+	);
+
+	lemlib::Chassis chassis(drivetrain, // drivetrain settings
+		lateral_controller, 			// lateral PID settings
+		angular_controller, 			// angular PID settings
+		sensors 						// odometry sensors
+	);
+
 void on_center_button() {}
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
-
+    pros::lcd::initialize();
+    chassis.calibrate();
+    pros::Task screen_task([&]() {
+        while (true) {
+            pros::lcd::print(0, "X: %f", chassis.getPose().x);
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y);
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);
+            pros::delay(20);
+        }
+    });
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+void autonomous() {
+	//initial
+		chassis.setPose(0, 0, 0);
+		hawkDown = !hawkDown;
+		hawk.set_value(hawkDown);
+	//move to blocks
+		chassis.moveToPoint(0, 10, 1000, {.maxSpeed = 80});
+		chassis.turnToHeading(-90, 500, {.maxSpeed = 80});
+		chassis.moveToPoint(-24, 10, 4000, {.maxSpeed = 80});
+	//suck blocks
+		pros::delay(1000);
+		tuahDown = !tuahDown;
+		tuah.set_value(tuahDown);
+		chain.move(-127);
+		chassis.moveToPoint(-50, 10, 4000);
+	//move to long goal
+		chassis.moveToPoint(-15, 10, 4000, {.forwards = false, .maxSpeed = 100});
+		chassis.turnToHeading(0, 1000);
+		chassis.moveToPoint(-15, 35, 1000);
+		chassis.turnToHeading(90, 2000);
+		chassis.moveToPoint(-48, 35, 8000, {.forwards = false, .maxSpeed = 100});
+	//deposit in long goal
+		therizzler.move(-127);
+		chain.move(127);
+		pros::delay(2000);
+	//push further in
+		chassis.moveToPoint(-15, 25, 1000, {.maxSpeed = 100});
+		chassis.turnToHeading(180, 1000);
+		chassis.moveToPoint(-15, 12, 1000);
+		chassis.turnToHeading(-90, 1000);
+		chassis.moveToPoint(-48, 35, 1000);
+		descoreDown = !descoreDown;
+		descore.set_value(descoreDown);
+}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 	pros::Controller controller(pros::E_CONTROLLER_MASTER);
 	while (true) {
